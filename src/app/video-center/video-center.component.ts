44867ce6dab45c842d6c8342dc7aa53f81fc657a
Mateo Leal <mateolegi@gmail.com>
2017-08-06 13:02:49 -0500

    CRUD complete

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -1,23 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from "app/video";
+import { VideoService } from "app/video.service";
 
 @Component({
   selector: 'app-video-center',
   templateUrl: './video-center.component.html',
-  styleUrls: ['./video-center.component.css']
+  styleUrls: ['./video-center.component.css'],
+  providers: [VideoService]
 })
 export class VideoCenterComponent implements OnInit {
 
-  videos: Video[] = [
-    {"_id": "1", "title": "Title 1", "url": "url 1", "description": "desc 1"},
-    {"_id": "2", "title": "Title 2", "url": "url 2", "description": "desc 2"},
-    {"_id": "3", "title": "Title 3", "url": "url 3", "description": "desc 3"},
-    {"_id": "4", "title": "Title 4", "url": "url 4", "description": "desc 4"}
-  ];
+  videos: Array<Video>;
+  selectedVideo: Video;
+  private hideNewVideo: boolean = true;
 
-  constructor() { }
+  constructor(private _videoService: VideoService) { }
 
   ngOnInit() {
+    this._videoService.getVideos()
+    .subscribe(resVideoData => this.videos = resVideoData);
+  }
+
+  onSelectVideo(video: any) {
+    this.selectedVideo = video;
+    this.hideNewVideo = true;
+  }
+
+  onSubmitAddVideo(video: Video) {
+    this._videoService.addVideo(video)
+    .subscribe(resNewVideo => {
+      this.videos.push(resNewVideo);
+      this.hideNewVideo = true;
+      this.selectedVideo = resNewVideo;
+    });
+  }
+
+  onUpdateVideoEvent(video: any) {
+    this._videoService.updateVideo(video).subscribe(resUpdateVideo => video = resUpdateVideo);
+    this.selectedVideo = null;
+  }
+
+  onDeleteVideoEvent(video: any) {
+    let videoArray = this.videos;
+    this._videoService.deleteVideo(video)
+    .subscribe(resDeletedVideo => {
+      for (let i = 0; i < videoArray.length; i++) {
+        if (videoArray[i]._id === video._id) {
+          videoArray.splice(i, 1);
+        }
+      }
+    });
+    this.selectedVideo = null;
+  }
+
+  newVideo() {
+    this.hideNewVideo = false;
   }
 
 }
---
+++ src/app/video.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { Video } from "app/video";
+
+@Injectable()
+export class VideoService {
+
+  private _getURL     = "/api/videos";
+  private _postURL    = "/api/video";
+  private _putURL     = "/api/video/";
+  private _deleteURL  = "/api/video/"; 
+
+  constructor(private _http: Http) { }
+
+  getVideos() {
+    return this._http.get(this._getURL)
+    .map((response: Response) => response.json());
+  }
+
+  addVideo(video: Video) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+    return this._http.post(this._postURL, JSON.stringify(video), options)
+    .map((response: Response) => response.json());
+  }
+
+  updateVideo(video: Video) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+    return this._http.put(this._putURL + video._id, JSON.stringify(video), options)
+    .map((response: Response) => response.json());
+  }
+
+  deleteVideo(video: Video) {
+    return this._http.delete(this._deleteURL + video._id)
+    .map((response: Response) => response.json());
+  }
+}
